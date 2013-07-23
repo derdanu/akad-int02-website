f9bc919e6d7874e0b7cae209ad1df451d128d906
Daniel Falkner <bmw@740i.de>
2013-07-23 13:43:54 +0200

    Buchungsanfrage. Mini Javascript fuer Errorhandling.

--- js/buchung.js
+++ js/buchung.js
@@ -1,13 +1,38 @@
 function checkForm() {
 
-
+	error = new Array();
+	
 	if (document.getElementById('reiseziel').selectedIndex == 0) {
 
-		alert(unescape("Bitte Reiseziel ausw%E4hlen"));
-		return false;
+		error.push('* Reiseziel');
 
 	}
 
+	if (document.getElementById('vorname').value == '') {
+
+		error.push('* Vorname');
+
+	}
+
+	if (document.getElementById('nachname').value == '') {
+
+		error.push('* Nachname');
+
+	}
+
+	if (document.getElementById('email').value == '') {
+
+		error.push('* Email');
+
+	}
+
+	if (error.length > 0) {
+
+		var error_header = 'Folgende Pflichfelder sind nicht ausgef%FCllt:\n\n';
+		alert(unescape(error_header + error.join('\n')));
+		return false;
+	}
+	
 	alert('Vielen Dank, Ihre Anfrage wird umgehend bearbeitet.');
 
 }
