671bccb5e81b6469f94d6732edafe73e3f03a64a
Daniel Falkner <bmw@740i.de>
2013-07-23 12:33:23 +0200

    Überprüfung ob Reiseziel ausgewaehlt ist.

--- js/buchung.js
+++ js/buchung.js
@@ -1,5 +1,13 @@
 function checkForm() {
 
-	alert('okidoki -- DUMMY');
+
+	if (document.getElementById('reiseziel').selectedIndex == 0) {
+
+		alert(unescape("Bitte Reiseziel ausw%E4hlen"));
+		return false;
+
+	}
+
+	alert('Vielen Dank, Ihre Anfrage wird umgehend bearbeitet.');
 
 }
