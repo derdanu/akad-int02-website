66f64866f48b4a4106856ec2240a40124ca1d0ec
Daniel Falkner <bmw@740i.de>
2013-07-23 08:11:49 +0200

    Meta Updates

--- index.php
+++ index.php
@@ -1,55 +1,54 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 <html>
 <head>
-	<title>Busunternehmen - Max Mustermann</title>
-	<meta name="language" content="de">
-	<meta name="keywords" content="Busunternehmen Max Musterman Ferien ERholung">
-	<meta name="description" content="Internetauftritt der Firma Max Mustemann">
-	<meta name="robots" content="index,follow">
-	<meta name="audience" content="alle">
-	<meta name="page-topic" content="Busunternehmen - Max Mustermann">
-	<meta name="revisit-after" CONTENT="7 days">	
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+        <title>Reiseunternehmen Max Mustermann - Bequeme Busreisen</title>
+        <meta name="language" content="de">
+        <meta name="keywords" content="Reiseunternehmen Max Mustermann, Ferien, Erholung, Busreisen, Urlaubsreise">
+        <meta name="description" content="Busreisen zu schönen Urlaubszielen. Das Reiseunternehmen Max Mustermann bringt Sie zu den schönsten Reisezielen in bequemen Reisebussen.">
+        <meta name="robots" content="index,follow">
+        <meta name="audience" content="alle">
+        <meta name="page-topic" content="Reiseunternehmen - Max Mustermann">
+        <meta name="revisit-after" CONTENT="7 days">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
 
-	<div id="menu">
-		<ul>
-			<li><a href="index.php">Home</a></li>
-			<li><a href="?seite=ueberuns">&Uuml;ber uns</a></li>
-			<li><a href="?seite=informationen">Informationen</a></li>
-			<li><a href="?seite=reiseziele">Reiseziele</a></li>
-			<li><a href="?seite=buchung">Buchung</a></li>
-			<li><a href="?seite=impressum">Impressum</a></li>
-		</ul>	
-	</div>
+        <div id="menu">
+                <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="?seite=ueberuns">&Uuml;ber uns</a></li>
+                        <li><a href="?seite=informationen">Informationen</a></li>
+                        <li><a href="?seite=reiseziele">Reiseziele</a></li>
+                        <li><a href="?seite=buchung">Buchung</a></li>
+                        <li><a href="?seite=impressum">Impressum</a></li>
+                </ul>
+        </div>
 
-	<div id="inhalt">
-	<?php 
-		switch ($_GET['seite']) {
+        <div id="inhalt">
+        <?php
+                switch ($_GET['seite']) {
 
-			case "ueberuns":
-				require("include/ueberuns.html");
-				break;
-			case "informationen":
-				require("include/informationen.html");
-				break;
-			case "reiseziele":
-				require("include/reiseziele.html");
-				break;
-			case "buchung":
-				require("include/buchung.html");
-				break;
-			case "impressum":
-				require("include/impressum.html");
-				break;
-			default:
-				require("include/inhalt.html");
-				
-		}
-	?>
-	</div>
+                        case "ueberuns":
+                                require("include/ueberuns.html");
+                                break;
+                        case "informationen":
+                                require("include/informationen.html");
+                                break;
+                        case "reiseziele":
+                                require("include/reiseziele.html");
+                                break;
+                        case "buchung":
+                                require("include/buchung.html");
+                                break;
+                        case "impressum":
+                                require("include/impressum.html");
+                                break;
+                        default:
+                                require("include/inhalt.html");
+
+                }
+        ?>
+        </div>
 
 </body>
 </html>
-
